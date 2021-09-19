b10b9c36487d3e1ebd274e18f86168778217eb05
montoyaguzman7 <j.montoya@bbva.com>
2021-09-18 23:40:39 -0500

    FEAT : Contenido para el componente Badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,9 +1,23 @@
 import React from "react";
+import confLogo from '../assets/img/confLogo.svg'
 
 class Badge extends React.Component {
     render() {
         return (
-            <div>badge works!</div>
+            <div>
+                <div>
+                    <img src={confLogo} alt="Logo de la conferencia"/>
+                </div>
+                <div>
+                    <img src="" alt="Avatar"/>
+                    <h1>Jose <br/> Montoya </h1>
+                </div>
+                <div>
+                    <p>Front end developer</p>
+                    <p>@montoyitag</p>
+                    <p>#platziConf</p>
+                </div>
+            </div>
         );
     }
 }
