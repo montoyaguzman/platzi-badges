2f4d2d76718ff4774b1b875cd96eb81ba564fbb0
montoyaguzman7 <j.montoya@bbva.com>
2021-09-18 23:03:05 -0500

    FEAT : HTML y JSX - React es JS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
+// With HTML native
+/*
+const element = document.createElement('h1');
+element.innerHTML = 'Hello, Platzi Badges!';
+const container = document.getElementById('app');
+container.appendChild(element);
+*/
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 const container = document.getElementById('app');
 
-const element = <h1>Hello, Platzi Badges</h1>;
-ReactDOM.render(element, container);
+const name = 'montoyitag';
+
+/* ejemplo 1 jsx */
+const htmlElement = React.createElement('h1', {}, 'Hola, Platzi Badges');
+const jsxElement = <h1>Hello, Platzi Badges - by {name}</h1>;
+
+/* ejemplo 1 jsx */
+const htmlElement2 = React.createElement(
+    'div', 
+    {}, 
+    React.createElement('h1', {}, 'jose montoya'),
+    React.createElement('p', {}, 'soy front end'),
+    );
+const jsxElement2 = (<div>
+    <h1>jose montoya</h1>
+    <p>soy front end</p>
+</div>);
+
+// ReactDOM.render(qué, donde);
+ReactDOM.render(jsxElement2, container);
